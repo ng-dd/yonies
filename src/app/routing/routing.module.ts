f9977f3deec6d5ff145420698f4d288ac10a0c70
Nadina Gerlach <nadina@nadina.la>
2017-10-28 11:36:35 -0700

    add routing module routes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileViewComponent } from '../profile-view/profile-view.component';
+import { ContentFeedComponent } from '../content-feed/content-feed.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: ContentFeedComponent },
+  { path: '', component: ContentFeedComponent },  
+  { path: 'profile', component: ProfileViewComponent },
+  { path: 'notfound', component: NotFoundComponent },
+  { path: '**', redirectTo: '/notfound' },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class RoutingModule { }
