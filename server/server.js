c0d13028a62c7e5a4958f33138ee6651cbd3389d
Nadina Gerlach <nadina@nadina.la>
2017-10-28 15:28:30 -0700

    socket io

--- server/server.js
+++ server/server.js
@@ -31,28 +31,87 @@ app.use(function(req, res, next){
   }
 })
 
-// Socket Chat Functionality - variables
+// Socket Chat Functionality - define variables
 const users = [];
 let username = '';
 let userid = '';
 let hostUserName = '';
 let hostUserid = '';
 
-// Socket IO connection
+// Connect via Socket IO session
 io.on('connection', function(socket) {
   // Listen for requests from users in room
+  socket.on('getUserSocketid', () => {
+    io.emit('userSocketid', userid);
+  });
+
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
   // Listen for request for host id
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
   // Listen for user to request chat
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
   // Listen for host to approve chat
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
   // Listen for new chat messages
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
   // End Chat
+  socket.on('', () => {
+    io.emit('', this.);
+  });
 
-  // Disconnect 
+  // Disconnect
+  socket.on('', () => {
+    io.emit('', this.);
+  });
+  
+  
+// sending to sender-client only
+socket.emit('message', "this is a test");
+
+// sending to all clients, include sender
+io.emit('message', "this is a test");
+
+// sending to all clients except sender
+socket.broadcast.emit('message', "this is a test");
+
+// sending to all clients in 'game' room(channel) except sender
+socket.broadcast.to('game').emit('message', 'nice game');
+
+// sending to all clients in 'game' room(channel), include sender
+io.in('game').emit('message', 'cool game');
+
+// sending to sender client, only if they are in 'game' room(channel)
+socket.to('game').emit('message', 'enjoy the game');
+
+// sending to all clients in namespace 'myNamespace', include sender
+io.of('myNamespace').emit('message', 'gg');
+
+// sending to individual socketid (server-side)
+socket.broadcast.to(socketid).emit('message', 'for your eyes only');
+
+// join to subscribe the socket to a given channel (server-side):
+socket.join('some room');
+
+// then simply use to or in (they are the same) when broadcasting or emitting (server-side)
+io.to('some room').emit('some event'):
+
+// leave to unsubscribe the socket to a given channel (server-side)
+socket.leave('some room');
 })
 
 app.use(express.static(path.join(__dirname, '../dist')))
