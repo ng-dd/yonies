c2151899b5ff04febec2cb890521950547e5d2c8
Daniel Kim <dkim099@gmail.com>
2017-10-30 12:05:56 -0700

    routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileViewComponent } from '../profile-view/profile-view.component';
 import { ContentFeedComponent } from '../content-feed/content-feed.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { NotFoundComponent } from '../not-found/not-found.component';
 import { VideoShareComponent } from '../video-share/video-share.component';
 
 const routes: Routes = [
   { path: 'home', component: ContentFeedComponent },
-  { path: '', component: ContentFeedComponent },  
+  { path: 'search', component: SearchBarComponent },
   { path: 'profile', component: ProfileViewComponent },
   { path: 'room', component: VideoShareComponent },
   { path: 'notfound', component: NotFoundComponent },
@@ -19,4 +20,5 @@ const routes: Routes = [
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
+
 export class RoutingModule { }
