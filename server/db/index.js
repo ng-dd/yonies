053f11f6b9cb183e4a4f5d797eb4ba8407246baf
Nadina Gerlach <nadina@nadina.la>
2017-11-02 17:38:03 -0700

    env file

--- server/db/index.js
+++ server/db/index.js
@@ -21,6 +21,7 @@ const sequelize = new Sequelize('yoniesDB', 'ngdd', 'plantlife', {
 
 // var sequelize = new Sequelize('postgres://gqhmxfxh:0moqyFAzfF1UOx3Nw8kKuly4cdpyH3f5@pellefant.db.elephantsql.com:5432/gqhmxfxh', {dialect: "postgres"});
   
+  
   sequelize.authenticate()
     .then(console.log('connected to the database!'))
     .catch(err => console.log('error connecting to database!', err));
