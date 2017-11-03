640b70dd5cd479b32b0021843a2246db8738650e
Daniel Kim <dkim099@gmail.com>
2017-11-03 01:23:10 -0700

    seed

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,11 @@
 var Sequelize = require('sequelize');
 require('dotenv').config();
+const Promise = require('bluebird');
 // var dbUrl = require('../../dburl');
 var seed = require('./seed/seed');
 
 const sequelize = new Sequelize('yoniesDB', 'ngdd', 'plantlife', {
+<<<<<<< ab8f5625144b476e30b411a5e9e2da16fb4fa8a9
 <<<<<<< 5f77fac3afe662038aa28fbd411242d64dd207e6
 <<<<<<< 056d5536e6cbe9b820aea97a3a45b706b7597751
   host: process.env.DATABASE_URL,
@@ -13,17 +15,22 @@ const sequelize = new Sequelize('yoniesDB', 'ngdd', 'plantlife', {
 =======
   host: 'yonies.cxdawuxv7dpb.us-west-2.rds.amazonaws.com',
 >>>>>>> rebaseing
+=======
+  host: process.env.DATABASE_URL,
+>>>>>>> seed
   port: 5432,
   dialect: 'postgres',
   dialectOptions: {
     ssl:'Amazon RDS'
   },
   pool: {
-    max: 5,
+    max: 20,
     min: 0,
     idle: 20000,
     acquire: 20000
   }});
+
+// var sequelize = new Sequelize('postgres://gqhmxfxh:0moqyFAzfF1UOx3Nw8kKuly4cdpyH3f5@pellefant.db.elephantsql.com:5432/gqhmxfxh', {dialect: "postgres"});
   
   sequelize.authenticate()
     .then(console.log('connected to the database!'))
@@ -43,9 +50,43 @@ const sequelize = new Sequelize('yoniesDB', 'ngdd', 'plantlife', {
   db.Post = require('../db/models/postModel')(sequelize, Sequelize);
   db.RoomStat = require('../db/models/roomStatModel')(sequelize, Sequelize);
 
-for (var key in db) {
-  console.log('@@@@@@@@@@@@SYNCING@@@@@@@@@@@: ', key)
-  db[key].sync({force: true})
+ const bs = [[seed.seedPosts, db.Post], [seed.seedUsers,db.User], [seed.seedLikes, db.Like], [seed.seedFriends, db.Friend]];
+ let counter = 0;
+ 
+const sync = () =>{
+  return new Promise((resolve, reject)=> {
+    for (var key in db) {
+      console.log('@@@@@@@@@@@@SYNCING@@@@@@@@@@@: ', key)
+      db[key].sync({force: true})
+    }
+    resolve()
+  })
 }
+
+sync()
+.then(()=>{
+  let counter = 1
+  bs.forEach((fn) => {
+    setTimeout(()=>{
+      console.log(fn[0], fn[1])
+      fn[0](fn[1])
+
+    }, 1000* counter)
+    counter++;
+  })
+})
+      // console.log(db[key], 'please')
+      // seed.bs[counter](db[key]);
+      // counter++;
+    //   setTimeout(function() {
+    //     seed.seedPosts(db)
+    //     seed.seedUsers(db.User)
+    //     seed.seedLikes(db.Like)
+    //     seed.seedFriends(db.Friend)
+    //  }, 3000)
+  //   })
+  // }
+
+
 
 module.exports = db;
