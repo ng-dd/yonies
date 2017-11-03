b4b9bfe8055529c4505a356f5c4314a88b98d6c4
Nadina Gerlach <nadina@nadina.la>
2017-11-03 06:58:42 -0700

    async login for splash page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+// Firebase
+import { AuthService } from './services/auth.services';
+import { UserService } from './services/user.service';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +16,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Yonies';
+  user: Observable<firebase.User>;
+
+  // Pass in fire auth and db
+  constructor(public afAuth: AngularFireAuth) {
+    
+    this.user = this.afAuth.authState;
+  }
 
 }
